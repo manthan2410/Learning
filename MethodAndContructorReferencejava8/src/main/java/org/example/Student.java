ec30549555338ac66c9be1675a5fa38cee3cd0b1
manthan2410 <44641100+manthan2410@users.noreply.github.com>
2025-02-26 12:44:25 +0530

    Add files via upload

---
+++ MethodAndContructorReferencejava8/src/main/java/org/example/Student.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Student {
+    public Student()
+    {
+        System.out.println("Implementation provided from constructor");
+    }
+    public static void doTask()
+    {
+        System.out.println("Implementation provided from static");
+    }
+    public void doWork()
+    {
+        System.out.println("Implementation provided from Non static");
+    }
+}
---
+++ MethodAndContructorReferencejava8/src/main/java/org/example/child.java
@@ -0,0 +1,8 @@
+package org.example;
+
+@FunctionalInterface
+public interface child {
+
+      void doTest();
+
+}
